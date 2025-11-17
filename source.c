c9c8f87486db01b317d8e8637d59fc4e188dd9e2
AvramenkoD <dsdautor227@gmail.com>
2025-11-17 12:24:47 +0300

    fix1

--- source.c
+++ source.c
@@ -1,135 +1,192 @@
 ﻿#include <stdio.h>
 #include <locale.h>
 #include <math.h>
-#define WIDTH 100
-#define HEIGHT 50
-#define SIZE1 10
+
+#define WIDTH   100     
+#define HEIGHT   50     
+#define SIZE1    10     
+
+
 typedef double (*TFunс)(double);
 
-double funcY(double);
-double funcV(double);
-int printTab(TFunс, double, double, double);
-int buildGraph(TFunс, double, double, double);
+double funcY(double x);
+double funcV(double x);
 
-int main() {
+int printTab(TFunс pFunc, double x1, double x2, double step);
+int buildGraph(TFunс f, double xStart, double xEnd, double step);
 
-	TFunс masFunc[2] = { funcY, funcV };
-	double masX[SIZE1];
-	int choise, opChoise;
-	double x1, x2, step;
+int main(void)
+{
+    setlocale(LC_ALL, "RUS");
 
-	setlocale(LC_ALL, "RUS");
+    TFunс masFunc[2] = { funcY, funcV };
+    int choice, opChoice,repeatF = 0;
+    double x1, x2, step;
 
-	puts("Выберите функцию: ");
-	puts("0) Y(x) = e ^ (cos x) * cos(sin(x))");
-	puts("");
-	puts("  	 |x>0.75			x+1");
-	puts("1)V(x) = |0 <= x <= 0.75	1 - pow(x,5)");
-	puts("         |x < 0			x + ln(|cos(x)|)");
-	scanf("%d", &choise);
+    puts("----------------------------------------");
+    puts("| Курсовой проект                      |");
+    puts("| Разработка программы анализа функций |");
+    puts("|                                      |");
+    puts("| Разработал: Д.А. Авраменко           |");
+    puts("| Группа: бИПТ-251                     |");
+    puts("| Руководитель О.В. Курипта            |");
+    puts("----------------------------------------");
 
-	puts("Выберите действие: ");
-	puts("1) Вычислить значение");
-	puts("2) Протабулировать функцию");
-	puts("3) Построить график функции");
-	scanf("%d", &opChoise);
+    system("pause");
 
-	switch (opChoise) {
-	case 1:
-		printf("\nВведите x: ");
-		scanf("%lf", &x1);
-		printf("y = %lf\n", masFunc[choise](x1));
-		break;
-	case 2:
-		printf("Введите диапазон и шаг через пробел: ");
-		scanf("%lf %lf %lf", &x1, &x2, &step);
-		printTab(masFunc[choise], x1, x2, step);
-		break;
-	case 3:
-		printf("Введите диапазон и шаг через пробел: ");
-		scanf("%lf %lf %lf", &x1, &x2, &step);
-		buildGraph(masFunc[choise], x1, x2, step);
-		break;
-	}
+    do
+    {
+        puts("\nВыберите функцию:");
+        puts("0) Y(x) = e^(cos(x)) * cos(sin(x))");
+        puts("");
+        puts("1) V(x) =");
+        puts("   | x > 0.75           -> x + 1");
+        puts("   | 0 <= x <= 0.75     -> 1 - x^5");
+        puts("   | x < 0              -> x + ln(|cos(x)|)");
+        printf("\nВаш выбор: ");
+        scanf("%d", &choice);
+
+        if (choice < 0 || choice > 1)
+        {
+            puts("Ошибка: выбран некорректный номер функции.");
+            return 1;
+        }
+
+        puts("\nВыберите действие:");
+        puts("1) Вычислить значение функции");
+        puts("2) Табулировать функцию");
+        puts("3) Построить график функции");
+        printf("\nВаш выбор: ");
+        scanf("%d", &opChoice);
+
+        switch (opChoice)
+        {
+        case 1:
+            printf("\nВведите x: ");
+            scanf("%lf", &x1);
+            printf("Результат: f(%.3lf) = %.10lf\n", x1, masFunc[choice](x1));
+            break;
+
+        case 2:
+            printf("Введите диапазон и шаг через пробел (x1 x2 h): ");
+            scanf("%lf %lf %lf", &x1, &x2, &step);
+            printTab(masFunc[choice], x1, x2, step);
+            break;
+
+        case 3:
+            printf("Введите диапазон и шаг через пробел (x1 x2 h): ");
+            scanf("%lf %lf %lf", &x1, &x2, &step);
+            buildGraph(masFunc[choice], x1, x2, step);
+            break;
+        default:
+            puts("Ошибка: выбран некорректный номер действия.");
+            break;
+        }
+
+        repeatF = 0;
+        printf("\n\nЖелаете повторить (0 - нет, иное число - да): ");
+        scanf("%d", &repeatF);
+        printf("\n\n\n");
+
+    } while (repeatF);
+    return 0;
 }
 
+/*------------------------------------------------------------
+ * Функция Y(x) = e^(cos(x)) * cos(sin(x))
+ * Непрерывная, определена при всех x ∈ R
+ *------------------------------------------------------------*/
 double funcY(double x)
 {
-	return exp(cos(x)) * cos(sin(x));
+    return exp(cos(x)) * cos(sin(x));
 }
 
-double funcV(double x) {
-	if (x > 0.75)
-		return x + 1;
-	else if (x >= 0)
-		return 1 - pow(x, 5);
-	else
-		return x + log(fabs(cos(x)));
+/*------------------------------------------------------------
+ * Кусочно-заданная функция V(x):
+ *  x > 0.75        -> x + 1
+ *  0 <= x <= 0.75  -> 1 - x^5
+ *  x < 0           -> x + ln(|cos(x)|)
+ *------------------------------------------------------------*/
+double funcV(double x)
+{
+    const double EPS = 1e-6;
+
+    if (x > 0.75)
+        return x + 1;
+    else if (x >= 0)
+        return 1 - pow(x, 5);
+    else {
+        /* проверка области допустимых значений */
+        if (fabs(cos(x)) < EPS) {
+            fprintf(stderr, "Ошибка: недопустимое значение x (cos(x) ≈ 0)\n");
+            return NAN;
+        }
+        return x + log(fabs(cos(x)));
+    }
 }
 
-int printTab(TFunс pfunc, double px1, double px2, double pstep) {
-	double temp;
-	printf("-----------------------------------\n");
-	printf("|    x    |       f(x)            |\n");
-	printf("-----------------------------------\n");
-	for (double i = px1; i <= px2; i += pstep) {
-		temp = pfunc(i);
-		printf("| %7.3lf | %21.10lf |\n", i, temp);
-	}
-	return 0;
+/*------------------------------------------------------------
+ * Функция вывода таблицы значений
+ *------------------------------------------------------------*/
+int printTab(TFunс pFunc, double x1, double x2, double step)
+{
+    double y;
+
+    printf("-------------------------------------------\n");
+    printf("|     x      |           f(x)             |\n");
+    printf("-------------------------------------------\n");
+
+    for (double i = x1; i <= x2; i += step) {
+        y = pFunc(i);
+        printf("| %9.3lf | %23.10lf |\n", i, y);
+    }
+
+    printf("-------------------------------------------\n");
+    return 0;
 }
 
-int buildGraph(TFunс f, double xStart, double xEnd, double step) {
-	char screen[WIDTH][HEIGHT];
-	double x, y[WIDTH];
-	double ymin = 0, ymax = 0;
-	double hx, hy;
-	int i, j;
-	int xz, yz;
+/*------------------------------------------------------------
+ * Функция построения графика функции в консоли
+ *------------------------------------------------------------*/
+int buildGraph(TFunс f, double xStart, double xEnd, double step)
+{
+    char screen[HEIGHT][WIDTH];
+    double x, y[WIDTH];
+    double ymin = 0, ymax = 0;
+    double hx, hy;
+    int i, j, xz, yz;
 
-	hx = (xEnd - xStart) / (WIDTH - 1);
+    hx = (xEnd - xStart) / (WIDTH - 1);
 
-	for (i = 0, x = xStart; i < WIDTH; ++i, x += hx) {
+    /* вычисление значений функции и поиск минимума/максимума */
+    for (i = 0, x = xStart; i < WIDTH; ++i, x += hx) {
+        y[i] = f(x);
+        if (y[i] < ymin) ymin = y[i];
+        if (y[i] > ymax) ymax = y[i];
+    }
 
-		y[i] = f(x);
-		if (y[i] < ymin)
-			ymin = y[i];
-		if (y[i] > ymax)
-			ymax = y[i];
-	}
+    hy = (ymax - ymin) / (HEIGHT - 1);
+    yz = (int)floor(ymax / hy + 0.5);
+    xz = (int)floor((0.0 - xStart) / hx + 0.5);
 
-	hy = (ymax - ymin) / (HEIGHT - 1);
-	yz = (int)floor(ymax / hy + 0.5);
-	xz = (int)floor((0. - xStart) / hx + 0.5);
+    /* построение осей и фона */
+    for (j = 0; j < HEIGHT; ++j)
+        for (i = 0; i < WIDTH; ++i)
+            screen[j][i] = (j == yz) ? '-' : (i == xz ? '|' : ' ');
 
+    /* нанесение точек функции */
+    for (i = 0; i < WIDTH; ++i) {
+        j = (int)floor((ymax - y[i]) / hy + 0.5);
+        if (j >= 0 && j < HEIGHT)
+            screen[j][i] = '*';
+    }
 
-	for (j = 0; j < HEIGHT; ++j) {
-		for (i = 0; i < WIDTH; ++i) {
+    /* вывод на экран */
+    for (j = 0; j < HEIGHT; ++j) {
+        for (i = 0; i < WIDTH; ++i)
+            putchar(screen[j][i]);
+        putchar('\n');
+    }
 
-			if (j == yz && i == xz)
-				screen[i][j] = '+';
-			else if (j == yz)
-				screen[i][j] = '-';
-			else if (i == xz)
-				screen[i][j] = '|';
-			else
-				screen[i][j] = ' ';
-
-		}
-	}
-
-	for (i = 0; i < WIDTH; ++i) {
-
-		j = (int)floor((ymax - y[i]) / hy + 0.5);
-		screen[i][j] = '*';
-
-	}
-
-	for (j = 0; j < HEIGHT; ++j) {
-
-		for (i = 0; i < WIDTH; ++i)  putchar(screen[i][j]);
-		putchar('\n');
-
-	}
-	return 0;
+    return 0;
 }
